5fb42b732607e319b66c3bda9b5a14e24004558d
dlim303 <deli4631@colorado.edu>
2020-02-22 18:14:20 -0700

    got the visual of the calculator done

--- Calc/Calc/ViewController.swift
+++ Calc/Calc/ViewController.swift
@@ -1,15 +1,6 @@
-//
-//  ViewController.swift
-//  Calc
-//
-//  Created by Dennis Lim on 2/22/20.
-//  Copyright © 2020 Dennis Lim. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
